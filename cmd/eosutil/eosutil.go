34bad8df9d0952c31b79a114424ea038ac60d7d4
gaozhengxin <zhengxingao@live.cn>
2020-12-15 17:10:24 +0800

    modify eos util

--- cmd/eosutil/eosutil.go
+++ cmd/eosutil/eosutil.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
 	"github.com/anyswap/CrossChain-Bridge/tokens/eos"
 )
 
+var pubkeyhex string
+
+func init() {
+	flag.StringVar(&pubkeyhex, "pubkey", "", "pubkey hex")
+}
+
 func main() {
-	pubkeyhex := "04c030e0d3e40fa06d94dae0122f0d472caaf0e27536286dfe4803d51d2db6d78e802a86fe77d319138fdbe3bda226782722b136b29c0c5ee80223dc143de1fa1b"
+	flag.Parse()
 	pubkey, err := eos.HexToPubKey(pubkeyhex)
 	if err != nil {
 		log.Fatal(err)
